2025dd80c4d0731bd6255d45517bd7b94c95dce3
Maksim Volkov <volkovmd.dev@mail.ru>
2022-05-13 10:14:28 +0300

    Initial Commit

---
+++ VK-SwiftUI/VK_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VK_SwiftUIApp.swift
+//  VK-SwiftUI
+//
+//  Created by Maksim Volkov on 13.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct VK_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
